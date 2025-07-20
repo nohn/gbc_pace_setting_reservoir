b96854ee0ed0ca484755a6e094b6377534703b01
Sebastian Nohn <sebastian@nohn.net>
2025-07-20 20:56:28 +0200

    - dynamic min/max based on target rate - use light for display status

--- gbc_pace_setter.py
+++ gbc_pace_setter.py
@@ -1,12 +1,15 @@
 from pybricks.pupdevices import Motor, ColorSensor
 from pybricks.parameters import Port
 from pybricks.tools import StopWatch
+from pybricks.hubs import EssentialHub
+from pybricks.parameters import Color
 
 TARGET_RATE       = 1.00 # Balls per second
-TARGET_TOLERANCE  = 0.05 # To avoid permament adjustments, allow 5% tolerance
+TARGET_TOLERANCE  = 0.10 # To avoid permament adjustments, allow 10% tolerance
 ADJUSTMENT_STEP   = 10
-MIN_SPEED         = 10
-MAX_SPEED         = 500
+INITAL_SPEED      = 125*TARGET_RATE 
+MIN_SPEED         = MIN_SPEED=INITAL_SPEED/2
+MAX_SPEED         = INITAL_SPEED*2
 CHECK_INTERVAL_MS = 100
 AVG_INTERVAL_SEC  = 5    # You may want to set this lower for smaller adjustment steps
 BALL_THRESHOLD    = 8
@@ -17,6 +20,7 @@ TARGET_RATE_LOWER = TARGET_RATE-(TARGET_RATE*TARGET_TOLERANCE)
 TARGET_RATE_UPPER = TARGET_RATE+(TARGET_RATE*TARGET_TOLERANCE)
 
 # Init peripherals
+hub = EssentialHub(broadcast_channel=None)
 motor = Motor(Port.A)
 sensor = ColorSensor(Port.B)
 
@@ -29,7 +33,7 @@ UNBLOCK_REVERSE_DEGREES = 20
 UNBLOCK_SPEED = -200
 
 # Initial Motor Speed
-motor_speed = 125*TARGET_RATE
+motor_speed = INITAL_SPEED
 motor.run(motor_speed)
 
 ball_count = 0
@@ -37,6 +41,9 @@ total_balls = 0
 total_time = 0.0
 cycle_balls = 0
 cycle_time = 0.0
+bpm_balls = 0
+bpm_time = 0.0
+bpm_avg_rate = False
 
 adjust_watch = StopWatch()
 cooldown_watch = StopWatch()
@@ -60,8 +67,10 @@ while True:
 
         total_balls += ball_count
         cycle_balls += ball_count
+        bpm_balls += ball_count
         total_time += elapsed
         cycle_time += elapsed
+        bpm_time += elapsed
         ball_count = 0
 
         if cycle_time >= AVG_INTERVAL_SEC:
@@ -75,10 +84,25 @@ while True:
 
             motor.run(motor_speed)
 
-            print(f"Cycle Rate: {cycle_avg_rate:.2f} bps | Target Rate: {TARGET_RATE_LOWER:.2f}<{TARGET_RATE:.2f}<{TARGET_RATE_UPPER:.2f} bps) | Next Cycle Speed: {motor_speed} deg/s | Avg Rate: {total_avg_rate:.2f} bps | Tot Time: {total_time:.1f}s | Tot Balls: {total_balls} | Avg Speed: {motor.angle()/total_time:.0f} deg/s")
+            print(f"CR: {cycle_avg_rate:.2f} bps | 60sAR: {bpm_avg_rate:.2f} bps | TAR: {total_avg_rate:.2f} bps | TR: {TARGET_RATE_LOWER:.2f}<{TARGET_RATE:.2f}<{TARGET_RATE_UPPER:.2f} bps) | NCS: {motor_speed} deg/s | AS: {motor.angle()/total_time:.0f} deg/s | TT: {total_time:.1f}s | TB: {total_balls}")
 
             cycle_balls = 0
             cycle_time = 0.0
+
+        if bpm_time >= 60:
+            bpm_avg_rate = bpm_balls / bpm_time
+            if bpm_avg_rate > TARGET_RATE_UPPER:
+                hub.light.on(Color.RED)
+                if bpm_avg_rate > 2*TARGET_RATE_UPPER:
+                    hub.light.blink(Color.RED, [500, 500])
+            elif bpm_avg_rate < TARGET_RATE_LOWER:
+                hub.light.on(Color.BLUE)
+                if bpm_avg_rate < 2*TARGET_RATE_LOWER:
+                    hub.light.blink(Color.BLUE, [500, 500])
+            else:
+                hub.light.on(Color.GREEN)
+            bpm_balls = 0
+            bpm_time = 0.0
 
     # Anti Blocking System
     if block_watch.time() >= BLOCK_CHECK_INTERVAL_MS:
@@ -88,9 +112,11 @@ while True:
 
         if delta < BLOCKED_THRESHOLD_DEGREES and motor_speed > 0:
             print("Motor seems blocked — attempting to unblock.")
+            hub.light.blink(Color.RED, [500, 500])
             motor.stop()
             motor.run_angle(UNBLOCK_SPEED, UNBLOCK_REVERSE_DEGREES)
             motor.run(motor_speed)
+            hub.light.blink(Color.BLUE, [500, 500])
 
         last_angle = current_angle
         block_watch.reset()
