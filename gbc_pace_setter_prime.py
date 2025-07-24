819943b89bf2fc2ed3523ebf640d45f090b0b9fa
Sebastian Nohn <sebastian@nohn.net>
2025-07-24 23:52:08 +0200

    set speed via buttons

--- gbc_pace_setter_prime.py
+++ gbc_pace_setter_prime.py
@@ -1,28 +1,27 @@
 from pybricks.pupdevices import Motor, ColorSensor
-from pybricks.parameters import Port
+from pybricks.parameters import Port, Color, Button, Side
 from pybricks.tools import StopWatch
-from pybricks.hubs import EssentialHub
-from pybricks.parameters import Color
+from pybricks.hubs import PrimeHub
 
-TARGET_RATE       = 1.00 # Balls per second
-TARGET_TOLERANCE  = 0.10 # To avoid permament adjustments, allow 10% tolerance
-ADJUSTMENT_STEP   = 10
-INITAL_SPEED      = 125*TARGET_RATE 
-MIN_SPEED         = MIN_SPEED=INITAL_SPEED/2
-MAX_SPEED         = INITAL_SPEED*2
+TARGET_RATE       = 0.95  # Balls per second
+TARGET_TOLERANCE  = 0.05  # 5% tolerance
+ADJUSTMENT_STEP   = TARGET_RATE * 5
+INITAL_SPEED      = 125 * TARGET_RATE
+MIN_SPEED         = INITAL_SPEED / 2
+MAX_SPEED         = INITAL_SPEED * 2
 CHECK_INTERVAL_MS = 100
-AVG_INTERVAL_SEC  = 5    # You may want to set this lower for smaller adjustment steps
+AVG_INTERVAL_SEC  = 5     # You may want to set this lower for smaller adjustment steps
 BALL_THRESHOLD    = 8
-BALL_COOLDOWN_MS  = 100  # Cooldown period for flank detection
+BALL_COOLDOWN_MS  = 100   # Cooldown period for flank detection
 
 # Calculate lower and upper rate boundaries using TARGET_TOLERANCE
-TARGET_RATE_LOWER = TARGET_RATE-(TARGET_RATE*TARGET_TOLERANCE)
-TARGET_RATE_UPPER = TARGET_RATE+(TARGET_RATE*TARGET_TOLERANCE)
+TARGET_RATE_LOWER = TARGET_RATE - (TARGET_RATE * TARGET_TOLERANCE)
+TARGET_RATE_UPPER = TARGET_RATE + (TARGET_RATE * TARGET_TOLERANCE)
 
 # Init peripherals
-hub = EssentialHub(broadcast_channel=None)
-motor = Motor(Port.A)
-sensor = ColorSensor(Port.B)
+hub = PrimeHub()
+motor = Motor(Port.B)
+sensor = ColorSensor(Port.A)
 
 # Init ABS
 block_watch = StopWatch()
@@ -45,9 +44,16 @@ bpm_balls = 0
 bpm_time = 0.0
 bpm_avg_rate = False
 
+display_watch = StopWatch()
 adjust_watch = StopWatch()
 cooldown_watch = StopWatch()
+display_watch.reset()
 cooldown_watch.reset()
+
+total_avg_rate = 0
+
+last_displayed_target_rate = None
+last_displayed_total_avg_rate = None
 
 while True:
     reflection = sensor.reflection()
@@ -60,7 +66,31 @@ while True:
         ball_count += 1
         cooldown_watch.reset()
 
-    # Check Cycle
+    buttons = hub.buttons.pressed()
+    if Button.LEFT in buttons:
+        TARGET_RATE = max(0.05, TARGET_RATE - 0.05)
+        print(f"TARGET_RATE decreased: {TARGET_RATE:.2f} bps")
+        ADJUSTMENT_STEP = TARGET_RATE * 5
+        INITAL_SPEED = 125 * TARGET_RATE
+        MIN_SPEED = INITAL_SPEED / 2
+        MAX_SPEED = INITAL_SPEED * 2
+        TARGET_RATE_LOWER = TARGET_RATE - (TARGET_RATE * TARGET_TOLERANCE)
+        TARGET_RATE_UPPER = TARGET_RATE + (TARGET_RATE * TARGET_TOLERANCE)
+
+    if Button.RIGHT in buttons:
+        TARGET_RATE = TARGET_RATE + 0.05
+        print(f"TARGET_RATE increased: {TARGET_RATE:.2f} bps")
+        ADJUSTMENT_STEP = TARGET_RATE * 5
+        INITAL_SPEED = 125 * TARGET_RATE
+        MIN_SPEED = INITAL_SPEED / 2
+        MAX_SPEED = INITAL_SPEED * 2
+        TARGET_RATE_LOWER = TARGET_RATE - (TARGET_RATE * TARGET_TOLERANCE)
+        TARGET_RATE_UPPER = TARGET_RATE + (TARGET_RATE * TARGET_TOLERANCE)
+
+    if last_displayed_target_rate != TARGET_RATE:
+        hub.display.text(f"{TARGET_RATE:.2f}")
+        last_displayed_target_rate = TARGET_RATE
+
     if adjust_watch.time() >= CHECK_INTERVAL_MS:
         elapsed = adjust_watch.time() / 1000
         adjust_watch.reset()
@@ -84,25 +114,27 @@ while True:
 
             motor.run(motor_speed)
 
-            print(f"CR: {cycle_avg_rate:.2f} bps | 60sAR: {bpm_avg_rate:.2f} bps | TAR: {total_avg_rate:.2f} bps | TR: {TARGET_RATE_LOWER:.2f}<{TARGET_RATE:.2f}<{TARGET_RATE_UPPER:.2f} bps) | NCS: {motor_speed} deg/s | AS: {motor.angle()/total_time:.0f} deg/s | TT: {total_time:.1f}s | TB: {total_balls}")
+            print(f"CR: {cycle_avg_rate:.3f} bps | 60sAR: {bpm_avg_rate:.3f} bps | TAR: {total_avg_rate:.3f} bps | TR: {TARGET_RATE_LOWER:.2f}<{TARGET_RATE:.2f}<{TARGET_RATE_UPPER:.2f} bps) | NCS: {motor_speed} deg/s | AS: {motor.angle()/total_time:.0f} deg/s | TT: {total_time:.1f}s | TB: {total_balls}")
 
             cycle_balls = 0
             cycle_time = 0.0
 
         if bpm_time >= 60:
             bpm_avg_rate = bpm_balls / bpm_time
+            bpm_balls = 0
+            bpm_time = 0.0
+
+        if bpm_avg_rate:
             if bpm_avg_rate > TARGET_RATE_UPPER:
                 hub.light.on(Color.RED)
-                if bpm_avg_rate > 2*TARGET_RATE_UPPER:
+                if bpm_avg_rate > 2 * TARGET_RATE_UPPER:
                     hub.light.blink(Color.RED, [500, 500])
             elif bpm_avg_rate < TARGET_RATE_LOWER:
                 hub.light.on(Color.BLUE)
-                if bpm_avg_rate < 2*TARGET_RATE_LOWER:
+                if bpm_avg_rate < 2 * TARGET_RATE_LOWER:
                     hub.light.blink(Color.BLUE, [500, 500])
             else:
                 hub.light.on(Color.GREEN)
-            bpm_balls = 0
-            bpm_time = 0.0
 
     # Anti Blocking System
     if block_watch.time() >= BLOCK_CHECK_INTERVAL_MS:
